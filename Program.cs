3c57964b2f020ada072f7e651acb083d5a2c08a9
SerovBox <booxds@gmail.com>
2022-11-29 22:03:19 +0300

    Add Home task Seminar C#_5

--- Program.cs
+++ Program.cs
@@ -1,123 +1,254 @@
-﻿//Console.Write("Input your number: ");
-//int number = Convert.ToInt32(Console.ReadLine());
-
-/*Console.WriteLine("Введите число, квадрат которого нужно найти: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int result = number * number;
-Console.WriteLine($"квадрат {number} равен {result}");
-*/
-
-//a = 25, b = 5 -> yes 
-//a = 2, b = 10 -> no
-//a = 9, b = -3 -> yes
-//a = -3, b = 9 -> no
-
-/*Console.WriteLine("Введите первое число: ");
-int number1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
-int number2 = Convert.ToInt32(Console.ReadLine());
-if (number2 * number2 == number1)
+﻿// Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. 
+//Найдите сумму отрицательных и положительных элементов массива.
+/*
+int[] CreateRandomArray (int size, int minValue, int maxValue) //метод создания массива с ранд элементами
 {
-    Console.WriteLine($"число {number1} является квадратом {number2}");
-}
-else
-{
-    Console.WriteLine($"число {number1} не являеться квадратом {number2}");
-}
-*/
-
-//Программа, которая на вход принимает число одно(N) а на выходе показывает все целые числа в промежутке от (-N до N).
-
-/*Console.WriteLine("Введите число N:");
-int num = Convert.ToInt32(Console.ReadLine());
-int current = num * (-1);
-while(current <= num)
-{
-    Console.Write(current + " ");
-    current++;
-}
-*/
-
-/* 
-Console.WriteLine("Введите трехзначное число");
-int number = Convert.ToInt32(Console.ReadLine());
-if(number > 99 && number < 1000)
-{
- int current = number % 10;
- Console.WriteLine($"Последняя цифра числа {number} - {current}");
-}
-else
-{
-    Console.WriteLine("Число не трехзначное ");
-}
-*/
-
-//Задача 2 
-//Напишите программу, которая на вход принимает два числа и выдаёт, 
-//какое число большее, а какое меньшее.
-
-/*Console.WriteLine("Введите два числа");
-int number1 = Convert.ToInt32(Console.ReadLine());
-int number2 = Convert.ToInt32(Console.ReadLine());
-if (number1 > number2)
-{
-    Console.WriteLine($"наибольшее число {number1}");
-}
-else 
-{
-    Console.WriteLine($"наибольшее число {number2}");
-}
-*/
-
-//Задача 4: Напишите программу, которая принимает на вход
-// три числа и выдает максимальное из этих чисел
-
-/*Console.WriteLine("Введите три числа");
-int number1 = Convert.ToInt32(Console.ReadLine());
-int number2 = Convert.ToInt32(Console.ReadLine());
-int number3 = Convert.ToInt32(Console.ReadLine());
-if (number1 > number2 && number1 > number3)
-{
-    Console.WriteLine($"Наибольшее число {number1}");
-}
-if (number2 > number1 && number2 > number3)
-{
-    Console.WriteLine($"Наибольшее число {number2}");
-}
-if (number3 > number2 && number3 > number2)
-{
-    Console.WriteLine($"Наибольшее число {number3}");
-}
-*/
-
-//Задача 6: Напишите программу, которая на вход принимает число
-// и выдает, является ли число четным 
-
-/*Console.WriteLine("Введите число");
-int number = Convert.ToInt32(Console.ReadLine());
-if (number % 2 == 0)
-{
-    Console.WriteLine($"Число четное {number}");
-}
-else
-{
-    Console.WriteLine($"Число не четное {number}");
-}
-*/
-
-// Задача 8: Напишите программу, которая на вход принимает число(N)
-// а на выходе показывает все четные числа от 1 до N
-
-/*Console.WriteLine("Введите число N");
-int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Четные числа от 1 до {number}");
-while (1 <= number)
-{
-    if (number % 2 == 0)
+    int [] newArray = new int [size];
+    for(int i = 0; i < size; i++)
     {
-    Console.WriteLine(number + ", ");
+        newArray[i] = new Random().Next(minValue, maxValue + 1);
     }
-    number++;
+    return newArray;
 }
+void ShowArray(int[] array) //демострация массива
+{
+    for(int i = 0; i < array.Length; i++)
+    Console.WriteLine(array[i] +" ");
+    Console.WriteLine();
+}
+int FindSummPos(int[] array)
+{
+    int sumPos = 0;
+    for(int i = 0; i < array.Length; i++)
+        if(array[i] > 0)
+            sumPos += array[i];
+    return sumPos;
+}
+int FindSummNeg(int[] array)
+{
+    int sunNeg = 0;
+    for(int i = 0; i < array.Length; i++)
+        if(array[i] < 0)
+            sunNeg += array[i];
+    return sunNeg;
+}
+int size_array = 12;
+int minValue_array = -9;
+int maxValue_array = 9;
+
+int[] CreatedArray = CreateRandomArray(size_array, minValue_array, maxValue_array);
+ShowArray(CreatedArray);
+Console.WriteLine($"Sum of positive elem is {FindSummPos(createdArray)}");
+int resultNeg = FindSummNeg(CreatedArray);
+Console.WriteLine($"Sum of negative elem is {resultNeg}");
 */
-//Решение так и не смог дадумать по последней задаче. Запускается "бесконечность чисел"
+
+//Найдите произведение пар чисел в одномерном массиве. 
+//Парой считаем первый и последний элемент, второй и предпоследний и т.д. 
+//Результат запишите в новом массиве.
+//[1 2 3 4 5] -> 5 8 3
+//6 7 3 6] -> 36 21
+/*
+int[] CreateRandomArray (int size, int minValue, int maxValue) //метод создания массива с ранд элементами
+{
+    int [] newArray = new int [size];
+    for(int i = 0; i < size; i++)
+    {
+        newArray[i] = new Random().Next(minValue, maxValue + 1);
+    }
+}
+void ShowArray(int[] array) //демострация массива
+{
+    for(int i = 0; i < array.Length; i++)
+    Console.WriteLine(array[i] +" ");
+    Console.WriteLine();
+}
+
+int[] FindMultiPar (int[] arrayCreated)
+{
+    int size2;
+    if (array.Length % 2 == 0)
+    size2 = size / 2;         // длина массива с произведением
+    else size2 = (array_size + 1) / 2;
+    int[] arrayMulti = new int[size2];
+    for(int i = 0; i < arrayCreated.Length / 2; i++)
+    {
+        arrayMulti[i] = arrayCreated[i] * arrayCreated[arrayCreated.Length - 1 - i];
+    }
+    if(arrayCreated.Length % 2 != 0)
+        arrayMulti[size2 - 1] = arrayCreated[size2 - 1];
+    
+    return arrayMulti;
+}
+Console.WriteLine("Input size if array: ");
+int array_size = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input min possible val if array: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input max possible val if array: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+
+int[] array1 = CreateRandomArray(array_size, minValue, maxValue);
+ShowArray(array1);
+int[] array2 = FindMultiPar(array1);
+ShowArray(array2);
+
+*/
+
+
+
+//Задайте одномерный массив из 123 случайных чисел. 
+//Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+/*
+int[] CreateRandomArray (int size, int minValue, int maxValue) //метод создания массива с ранд элементами
+{
+    int[] newArray = new int [size];
+    for(int i = 0; i < size; i++)
+    {
+        newArray[i] = new Random().Next(minValue, maxValue + 1);
+        // + Random().NextDouble(); //от 0 до 1
+        //Math.Round(newArray[i], 3);
+    }
+    return newArray;
+}
+void ShowArray(int[] array) //демострация массива
+{
+    for(int i = 0; i < array.Length; i++)
+    Console.Write(array[i] +" ");
+    Console.WriteLine();
+}
+
+int Count1099(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+        if(array[i] > 9 && array[i] < 100)
+        count++;
+    return count;
+}
+
+Console.WriteLine("Input min possible val if array: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input max possible val if array: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+
+int size_array = 12;
+int[] arrayCreated = CreateRandomArray(size_array, minValue, maxValue);
+ShowArray(arrayCreated);
+int res = Count1099(arrayCreated);
+Console.WriteLine($"Count of elem between 10 and 99 is {res}");
+*/
+
+//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+/*
+int[] CreateRandomArray (int size, int start, int end) 
+{
+    int[] newArray = new int [size];
+    for(int i = 0; i < size; i++)
+    {
+        newArray[i] = new Random().Next(start, end + 1);        
+    }
+    return newArray;
+}
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    Console.Write(array[i] +" ");
+    Console.WriteLine();
+}
+
+int CountOfEven(int[] array1)
+{
+    int count = 0;
+    for(int i = 0; i < array1.Length; i++)
+        if(array1[i] % 2 == 0)
+        count++;        
+    return count;
+}
+int[] array1 = new int[3];
+array1 = CreateRandomArray(3,100,1000);
+ShowArray(array1);
+Console.WriteLine("numbers : " + CountOfEven(array1));
+*/
+
+//Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+/*
+void CreateRandomArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(1,10);
+        }
+}
+void ShowArray (int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    Console.Write(numbers[i] + " ");
+    Console.WriteLine();
+}
+
+Console.WriteLine("Input Array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+CreateRandomArray(numbers);
+Console.WriteLine("Your array: ");
+ShowArray(numbers);
+int sum = 0;
+
+for (int z = 0; z < numbers.Length; z+=2)
+    sum = sum + numbers[z];
+
+Console.WriteLine($"All {numbers.Length}, summ on the uneven = {sum}");
+*/
+
+
+//Задача 38: Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
+/*
+int[] CreateRandomArray (int[] numbers) 
+{
+    //int[] newArray = new int [size];
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(10, 1000);        
+    }
+    return numbers;
+}
+void ShowArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    Console.Write(numbers[i] +" ");
+    Console.WriteLine();
+}
+
+Console.WriteLine("Input array");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+CreateRandomArray(numbers);
+Console.WriteLine("Your Array: ");
+ShowArray(numbers);
+int min = Int32.MaxValue;
+int max = Int32.MinValue;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > max)
+        {
+            max = numbers[i];
+        }
+    if (numbers[i] < min)
+        {
+            min = numbers[i];
+        }
+}
+
+Console.WriteLine($"All {numbers.Length} numbers. Maximum = {max}, Minimum = {min}");
+Console.WriteLine($"Difference = {max - min}");
+*/
+
